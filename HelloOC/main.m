c12099b12a97909549a631626a29c7c34bbee906
grass <grassswwang@tencent.com>
2020-11-07 11:25:17 +0800

    Initial Commit

---
+++ HelloOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloOC
+//
+//  Created by grass on 2020/11/7.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
