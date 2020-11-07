155a427671d304247b7cdbcec7ec293853003a47
grass <grassswwang@tencent.com>
2020-11-07 15:38:48 +0800

    添加一个学习oc的工程

--- HelloOC/main.m
+++ HelloOC/main.m
@@ -11,6 +11,11 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        int value1, value2, sum;
+        value1 = 20;
+        value2 = 20;
+        sum = value1 + value2;
+        NSLog(@"%i plus %i is %i", value1,value2, sum);
     }
     return 0;
 }
